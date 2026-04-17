9e5b233f5669a7032485afb984560fbdb80b36a2
Kennny7 <khushalpareta9@gmail.com>
2026-04-17 16:04:29 +0530

    update implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://kennny7.github.io',
   base: '/',
   integrations: [tailwind()],
   build: {
-    assets: 'assets'
+    assets: 'assets',
+    inlineStylesheets: 'auto'
   }
 });
